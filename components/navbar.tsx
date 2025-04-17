d9e61b5ba8a0c2f65afd6b14314db5c9eb05e9d6
Joseph <kutumane.joseph480@gmail.com>
2025-04-17 14:43:53 +0200

    Navbar update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,12 +8,20 @@ const Navbar = () => {
       <div className="max-w-prose mx-auto flex justify-between flex-col sm:flex-row">
         <h1 className="text-3xl font-bold text-gray-600 grid place-content-center mb-2 md:mb-0">
           <Link
+            href="/"
+            className="text-cyan-600 hover:text-gray-400 no-underline "
+          >
+            Home page
+          </Link>
+        </h1>
+        <h2>
+          <Link
             href="/bloglist"
             className="text-cyan-600 hover:text-gray-400 no-underline "
           >
-            Blog
+            Blog page
           </Link>
-        </h1>
+        </h2>
       </div>
     </nav>
   );
